d3fc3b8c9ee19ff2fb0ba07aa9469d4547a0bfc1
Ch1ckenMax <59310578+Ch1ckenMax@users.noreply.github.com>
2021-03-12 03:54:17 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ for((i=1;i<=$2;i++))
 do
   if [ -e $3/input$1_$i.txt ]
   then
-    ./$1.exe <$3/input$1_$i.txt 1>output.txt 2>>testError.txt
+    ./$1 <$3/input$1_$i.txt 1>output.txt 2>>testError.txt
     difference="`diff output.txt $3/output$1_$i.txt`"
     rm output.txt
     if [ ${#difference} -eq 0 ]
